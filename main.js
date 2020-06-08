968bf5db3e68855e8b94f58b75b8a22a8d913411
zack <kaisenaiko@gmail.com>
2020-06-08 20:01:55 +0700

    test

---
+++ main.js
@@ -0,0 +1,15 @@
+$("#myinput").keyup(function() {
+    var val = $.trim(this.value);
+    if (val === "")
+        $('img').show();
+    else {
+        $('img').hide();
+        $("img[alt*=" + val + " i]").show();
+    }
+});
+
+$(".img").wrap('<div class="alt-wrap"/>');
+
+$(".img").each(function() {
+    $(this).after('<p class="alt">' + $(this).attr('alt') + '</p>');
+})
